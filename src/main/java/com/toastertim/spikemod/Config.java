602d74c6bd439d98d7655f07e456cd8f54f64d02
toastertim <toastertim2two@gmail.com>
2017-03-06 14:08:49 -0700

    Attempt to fix config with different style to do so, still doesnt create config file

--- src/main/java/com/toastertim/spikemod/Config.java
+++ src/main/java/com/toastertim/spikemod/Config.java
@@ -25,7 +25,7 @@ public class Config {
             initGenConfig(cfg);
         } catch (Exception e1) {
             SpikeMod.logger.log(Level.ERROR, "Problem loading config file!", e1);
-
+            System.out.println("Spikemod config error" + e1);
         } finally {
             if (cfg.hasChanged()) {
                 cfg.save();
@@ -36,6 +36,7 @@ public class Config {
     private static void initGenConfig(Configuration cfg) {
         cfg.addCustomCategoryComment(CATEGORY_GENERAL, "General configuration");
         dropsXP = cfg.getBoolean(CATEGORY_GENERAL, "xpDrop", dropsXP, "Set to false for to change spike damage to not drop xp");
+        //dropsXP = cfg.get(CATEGORY_GENERAL, "xpDrop", dropsXP, "Set to false for to change spike damage to not drop xp").getBoolean();
     }
 }
 
--- src/main/java/com/toastertim/spikemod/proxy/CommonProxy.java
+++ src/main/java/com/toastertim/spikemod/proxy/CommonProxy.java
@@ -2,6 +2,7 @@ package com.toastertim.spikemod.proxy;
 
 import com.toastertim.spikemod.Config;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -16,8 +17,14 @@ public class CommonProxy {
     public static Configuration config;
 
     public void preInit(FMLPreInitializationEvent event){
+       /* Doesnt make config file
         File directory = event.getModConfigurationDirectory();
         File configFile = new File(directory.getPath(), "spikemod.cfg");
+        config = new Configuration(configFile);
+        Config.readConfig(); */
+
+       //Doesnt work either??
+        File configFile = new File(Loader.instance().getConfigDir(), "SpikeMod.cfg");
         config = new Configuration(configFile);
         Config.readConfig();
     }
